3754e70b39b931241082d0f17d3d1097d0fe41f2
~Tijani Fareed~ <fareedtijani2810@gmail.com>
2024-11-15 15:54:59 +0100

    end point for get a pending seller done

--- demo/src/main/java/com/freddie/marketplace/config/SecurityConfig.java
+++ demo/src/main/java/com/freddie/marketplace/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("register", "loginPage", "create_adminAccount", "loginByAdmin", "getAllPendingSellers")
+                        .requestMatchers("register", "loginPage", "create_adminAccount", "loginByAdmin", "getAllPendingSellers", "getApendingSeller")
                         .permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
